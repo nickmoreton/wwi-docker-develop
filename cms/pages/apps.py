e218859126321446ef291af2bb5f7fc75c9c1fda
Nick Moreton <nickmoreton@me.com>
2022-01-25 22:09:45 +0000

    move apps

--- cms/pages/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PagesConfig(AppConfig):
-    name = "pages"
-
-    # Un-commnet to use the import prefilters
-    # def ready(self):
-    #     from . import prefilters
